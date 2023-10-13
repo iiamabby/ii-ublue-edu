641be4731eb87fe438b24a9424958c9c99f45632
= <abby.crimlis@outlook.com>
2023-10-13 13:45:13 +1300

    added minecraft

---
+++ config/scripts/minecraft.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+apt get install -y java-17-openjdk
+
+mkdir minecraftforge && \
+cd minecraftforge && \
+wget "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.2.0/forge-1.20.1-47.2.0-mdk.zip" -O temp.zip && \
+unzip temp.zip && \
+rm temp.zip
+./gradlew build
+cd ~
