da7a58d22e71022172d153393debff2baf370049
= <abby.crimlis@outlook.com>
2023-10-24 13:05:51 +1300

    usr should exist at this point

--- config/scripts/example.sh
+++ config/scripts/example.sh
@@ -11,3 +11,11 @@ echo 'Scripts here will run during build if specified in recipe.yml'
 # Install Java
 rpm-ostree install -y java-17-openjdk
 export PATH="/usr/lib/jvm/java-17-openjdk-17.0.8.0.7-1.fc38.x86_64/bin:$PATH"
+echo $PATH
+
+mkdir  /usr/local/bin/minecraftforge && \
+cd /usr/local/bin/minecraftforge && \
+wget "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.2.0/forge-1.20.1-47.2.0-mdk.zip" -O temp.zip && \
+unzip temp.zip && \
+ls && \ 
+rm temp.zip
